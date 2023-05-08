746d52c9f30f110ea8a929ade96974c9467323bf
ssai2001 <mohansaip2001@gmail.com>
2023-05-08 17:05:46 +0530

    make changes

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8yee#3*6&826o7-v6)hfvo9=7(^3#dhg7ila==d+#fkg7mz!_r"
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,8 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "psaimohancs@gmail.com"
-EMAIL_HOST_PASSWORD = "oltauimbhabblfyx"
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 EMAIL_USE_TLS = True
 
 CSRF_TRUSTED_ORIGINS = ['https://*.gcpbloodbank.live./','https://*.127.0.0.1','https://*.gcpbloodbank.live']
