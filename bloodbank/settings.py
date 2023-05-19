79eb035f4b9613e27d4598405efd806875d2d216
ssai2001 <mohansaip2001@gmail.com>
2023-05-19 10:09:25 +0530

    for deployment in pythonanywhere

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,8 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 
-EMAIL_HOST_PASSWORD = 
+EMAIL_HOST_USER = config("USER_MAIL")
+EMAIL_HOST_PASSWORD = config("USER_PASSWORD")
 EMAIL_USE_TLS = True
 
 CSRF_TRUSTED_ORIGINS = ['https://*.gcpbloodbank.live./','https://*.127.0.0.1','https://*.gcpbloodbank.live']
